ff6220d3e88f9f623cd89f00458e866f5beea917
Mindaugas Dirgincius <Mindaugas.Dirgincius@bentley.com>
2023-11-07 16:17:43 +0200

    Simplify query

--- src/query.ts
+++ src/query.ts
@@ -1,4 +1,4 @@
-import { ElementAspect, IModelDb } from "@itwin/core-backend";
+import { ElementAspect, ExternalSourceAspect, IModelDb } from "@itwin/core-backend";
 import { DbResult } from "@itwin/core-bentley";
 import { ElementAspectProps, QueryRowFormat } from "@itwin/core-common";
 import { logStats } from "./monitor";
@@ -59,86 +59,56 @@ export function queryElementAspects(flags: Flags) {
 }
 
 async function queryElementAspectsInPages(pageSize: number, timeout: () => Promise<void>, cleanDbCache: (iModelDb: IModelDb) => void, callback: (aspect: ElementAspect) => void, iModelDb: IModelDb) {
-    const aspectClassNameIdMap = new Set<{ schemaName: string; className: string; }>();
-
-    console.log("Querying class infos");
-    const getAspectClassesSql = `SELECT DISTINCT (ec_classname(ECClassId, 'c')) as className, (ec_classname(ECClassId, 's')) as schemaName FROM ${ElementAspect.classFullName}`;
-    const queryReader = iModelDb.createQueryReader(getAspectClassesSql);
-    for await (const rowProxy of queryReader) {
-        const row = rowProxy.toRow();
-        aspectClassNameIdMap.add({ schemaName: row.schemaName, className: row.className });
-    }
-    console.log("Queried class infos");
-
-    for (const { schemaName, className } of aspectClassNameIdMap) {
-        const classFullName = `${schemaName}:${className}`;
-
-        let currentPage = 0;
-        const getAspectPropsSql = `SELECT * FROM ONLY [${schemaName}]:[${className}]`;
-        while (true) {
-            const queryReader = iModelDb.createQueryReader(
-                getAspectPropsSql,
-                undefined,
-                {
-                    rowFormat: QueryRowFormat.UseJsPropertyNames,
-                    limit: { count: pageSize, offset: pageSize * currentPage }
-                }
-            );
-
-
-            let currentPageSize = 0;
-            for await (const rowProxy of queryReader) {
-                const row = rowProxy.toRow();
-                const aspectProps: ElementAspectProps = { ...row, classFullName, className: undefined }; // add in property required by EntityProps
-                const aspectEntity = iModelDb.constructEntity<ElementAspect>(aspectProps);
-                ++currentPageSize;
-
-                callback(aspectEntity);
-            }
-
-            await timeout();
-            cleanDbCache(iModelDb);
-
-            ++currentPage;
-
-            if (currentPageSize < pageSize) {
-                break;
-            }
-        }
-    }
-}
-
-async function queryElementAspectsWithoutPaging(timeout: () => Promise<void>, cleanDbCache: (iModelDb: IModelDb) => void, callback: (aspect: ElementAspect) => void, iModelDb: IModelDb) {
-    const aspectClassNameIdMap = new Set<{ schemaName: string; className: string; }>();
-
-    console.log("Querying class infos");
-    const getAspectClassesSql = `SELECT DISTINCT (ec_classname(ECClassId, 'c')) as className, (ec_classname(ECClassId, 's')) as schemaName FROM ${ElementAspect.classFullName}`;
-    const queryReader = iModelDb.createQueryReader(getAspectClassesSql);
-    for await (const rowProxy of queryReader) {
-        const row = rowProxy.toRow();
-        aspectClassNameIdMap.add({ schemaName: row.schemaName, className: row.className });
-    }
-    console.log("Queried class infos");
-
-    for (const { schemaName, className } of aspectClassNameIdMap) {
-        const classFullName = `${schemaName}:${className}`;
-
-        const getAspectPropsSql = `SELECT * FROM ONLY [${schemaName}]:[${className}]`;
+    let currentPage = 0;
+    const getAspectPropsSql = `SELECT * FROM ONLY ${ExternalSourceAspect.classFullName}`;
+    while (true) {
         const queryReader = iModelDb.createQueryReader(
             getAspectPropsSql,
             undefined,
-            { rowFormat: QueryRowFormat.UseJsPropertyNames }
+            {
+                rowFormat: QueryRowFormat.UseJsPropertyNames,
+                limit: { count: pageSize, offset: pageSize * currentPage }
+            }
         );
 
+
+        let currentPageSize = 0;
         for await (const rowProxy of queryReader) {
             const row = rowProxy.toRow();
-            const aspectProps: ElementAspectProps = { ...row, classFullName, className: undefined }; // add in property required by EntityProps
+            const aspectProps: ElementAspectProps = { ...row, classFullName: ExternalSourceAspect.classFullName, className: undefined }; // add in property required by EntityProps
             const aspectEntity = iModelDb.constructEntity<ElementAspect>(aspectProps);
+            ++currentPageSize;
 
             callback(aspectEntity);
         }
 
         await timeout();
         cleanDbCache(iModelDb);
+
+        ++currentPage;
+
+        if (currentPageSize < pageSize) {
+            break;
+        }
     }
+}
+
+async function queryElementAspectsWithoutPaging(timeout: () => Promise<void>, cleanDbCache: (iModelDb: IModelDb) => void, callback: (aspect: ElementAspect) => void, iModelDb: IModelDb) {
+    const getAspectPropsSql = `SELECT * FROM ONLY ${ExternalSourceAspect.classFullName}`;
+    const queryReader = iModelDb.createQueryReader(
+        getAspectPropsSql,
+        undefined,
+        { rowFormat: QueryRowFormat.UseJsPropertyNames }
+    );
+
+    for await (const rowProxy of queryReader) {
+        const row = rowProxy.toRow();
+        const aspectProps: ElementAspectProps = { ...row, classFullName: ExternalSourceAspect.classFullName, className: undefined }; // add in property required by EntityProps
+        const aspectEntity = iModelDb.constructEntity<ElementAspect>(aspectProps);
+
+        callback(aspectEntity);
+    }
+
+    await timeout();
+    cleanDbCache(iModelDb);
 }
